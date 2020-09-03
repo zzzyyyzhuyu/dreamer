1e641ca52125973afc0bbe589501182e763c37ba
zzzyyyzhuyu <984154534@qq.com>
2020-09-03 15:10:25 +0800

    init

---
+++ src/main/java/com/wimp/dreamer/DreamerApplication.java
@@ -0,0 +1,16 @@
+package com.wimp.dreamer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhuyu
+ */
+@SpringBootApplication
+public class DreamerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DreamerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wimp/dreamer/DreamerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wimp.dreamer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
